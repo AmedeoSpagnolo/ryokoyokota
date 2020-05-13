fab21b1b739a2c97b6699f2b942c591aba78ab77
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2020-05-13 16:16:41 +0900

    antenna

--- assets/scripts/_escaperakuten.js
+++ assets/scripts/_escaperakuten.js
@@ -2,8 +2,14 @@ $('document').ready(function(){
 
   window.addEventListener('keydown', moveSelection);
   var w = parseInt($(".canvas").width())
+  var h = parseInt($(".canvas").height())
   var r = parseInt($("#ryoko").width())
   $("#ryoko").css("left", w/2 - r/2)
+
+  antenna()
+  setInterval(function(){
+    antenna()
+  }, 1000);
 
   function leftArrowPressed() {
       var l = parseInt($("#ryoko").css("left"))
@@ -35,5 +41,51 @@ $('document').ready(function(){
       break;
     }
   };
+  function antenna(){
+    // $(".canvas").add('img')
+    var $ant = $("<img class='antenna' src='http://placekitten.com/69/71'></img>")
+    var _w = 30
+    $ant.css({
+      "left": parseInt(Math.random()*(w-_w)) ,
+      "position": "absolute",
+      "top": 0,
+      "width": _w
+    })
+    $(".canvas").append($ant)
+    $ant.animate({top: $(".canvas").height(),}, Math.random() + 5000, function(){
+      $(this).remove();
+    });
+  }
 
 });
+
+
+ // id="antenna" src="http://placekitten.com/69/71">
+ // function fallingSnow() {
+ //
+ //     var $snowflakes = $(), qt = 20;
+ //
+ //     for (var i = 0; i < qt; ++i) {
+ //         var $snowflake = $('<div class="snowflakes"></div>');
+ //         $snowflake.css({
+ //             'left': (Math.random() * $('#site').width()) + 'px',
+ //             'top': (- Math.random() * $('#site').height()) + 'px'
+ //         });
+ //         // add this snowflake to the set of snowflakes
+ //         $snowflakes = $snowflakes.add($snowflake);
+ //     }
+ //     $('#snowZone').prepend($snowflakes);
+ //
+ //     $snowflakes.animate({
+ //         top: "500px",
+ //         opacity : "0",
+ //     }, Math.random() + 5000, function(){
+ //         $(this).remove();
+ //         // run again when all 20 snowflakes hit the floor
+ //         if (--qt < 1) {
+ //             fallingSnow();
+ //         }
+ //     });
+ // }
+ // fallingSnow();
+ //
